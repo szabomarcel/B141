896a1df1cd8cf76b9b48962a8b3caf5ef47dc1c8
szabomarcel <szabomarcell34@gmail.com>
2023-09-12 14:38:41 +0200

    feladat

---
+++ Berek2020/Berek.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek2020
+{
+    internal class Berek
+    {
+        string nev; 
+        string neme; 
+        string resz1eg; 
+        int belepes;
+        int ber;
+        public string Nev { get => nev; set => nev = value; }
+        public string Neme { get => neme; set => neme = value; }
+        public string Resz1eg { get => resz1eg; set => resz1eg = value; }
+        public int Belepes { get => belepes; set => belepes = value; }
+        public int Ber { get => ber; set => ber = value; }
+
+        public Berek(string nev, string neme, string resz1eg, int belepes, int ber)
+        {
+            this.Nev = nev;
+            this.Neme = neme;
+            this.Resz1eg = resz1eg;
+            this.Belepes = belepes;
+            this.Ber = ber;
+        }
+        public Berek(string beolvas) 
+        {
+            string[] strings = beolvas.Split(';');
+            this.Nev = strings[0];
+            this.Neme = strings[1];
+            this.Resz1eg = strings[2];
+            this.Belepes = int.Parse(strings[3]);
+            this.Ber = int.Parse(strings[4]);
+        }
+    }
+}
---
+++ Berek2020/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Berek2020
+{
+    internal class Program
+    {
+        static List<Berek> berek = new List<Berek>();
+        static void Main(string[] args)
+        {
+            feladat3();
+            feladat4();
+            feladat5();
+            feladat6();
+            feladat7();
+            Console.ReadLine();
+        }
+
+        private static void feladat7()
+        {
+            Console.WriteLine("\n7. Feladat");
+            foreach (var item in berek.FindAll(z => z.Ber == 0).GroupBy(a => a.Nev).Select(b => new { ok = b.Key, db = b.Count()}))
+            {
+                if (item.db > 2)
+                {
+                    Console.WriteLine($"\t{item.ok} : {item.db}");
+                }
+            }
+            Console.WriteLine("Statisztikák: ");
+        }
+
+        private static void feladat6()
+        {
+            Console.WriteLine("\n6. Feladat");
+            foreach (var item in berek.Select(a => a.Belepes))
+            {
+                Console.WriteLine($"\t{item}");
+            }
+            Console.WriteLine("A legtöbbet kereső dolgozó a megadott részlegen: ");
+        }
+
+        private static void feladat5()
+        {
+            Console.WriteLine("\n5. Feladat");
+            foreach(Berek item in berek.FindAll(a => a.Equals("beszerzés")))
+            {
+                Console.WriteLine($"Kérem egy részleg nevét: \t{item.Belepes.ToString()}");
+            }
+            Console.WriteLine("Kérem egy részleg nevét: ");
+        }
+
+        private static void feladat4()
+        {
+            Console.WriteLine("\n4. Feladat");
+            foreach (Berek item in berek.FindAll(a => a.Equals("")))
+            {
+                Console.WriteLine($"Bérek átlaga: \t{item.Ber.ToString()}. eFT");
+            }
+            Console.WriteLine($"Bérek átlaga: ");
+        }
+
+        private static void feladat3()
+        {
+            Console.WriteLine("3. Feladat");
+            Console.WriteLine($"Dolgozók száma: " + berek.Count());
+        }
+
+        private static void adatbeolvas()
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(@"berek2020"))
+                {
+                    file.ReadLine();
+                    while (!file.EndOfStream)
+                    {
+                        berek.Add(new Berek(file.ReadLine()));
+
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
