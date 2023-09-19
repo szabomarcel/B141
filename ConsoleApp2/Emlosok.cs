b69d72b4cda8e01cacdd1a34638240b2d1fbcf09
szabomarcel <szabomarcell34@gmail.com>
2023-09-19 11:09:05 +0200

    odaianyag

---
+++ ConsoleApp2/Emlosok.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Emlosok
+    {
+        //tulajdonságok -> viselkedés
+        public string Nev;
+        public string faj;
+        public int labszam;
+        public double suly;
+        public string nem;
+        string hang;
+
+        //Hang megadás
+        public String getHangotAd()
+        {
+            return hang;
+        }
+
+        public void setHangotAd(string hang)
+        {
+            this.hang = hang;
+        }
+        public String Halad()
+        {
+            return "elöre";
+        }
+        public override string ToString()
+        {
+            return this.Nev;
+        }
+        // Konstruktor létrehozása. 
+        public Emlosok(string nev, string faj, int labszam, double suly, string nem, string hang)
+        {
+            this.Nev = nev;
+            this.faj = faj;
+            this.labszam = labszam;
+            this.suly = suly;
+            this.nem = nem;
+            this.hang = hang;
+        }
+    }
+}
---
+++ ConsoleApp2/Kutya.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Kutya : Emlosok
+    {
+        public Kutya(string nev, double suly, string nem) : base(nev, "Kutya", 4, suly, nem, "vaúúú-vaúúú")
+        {
+        }
+    }
+}
