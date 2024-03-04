9cada53067abdd86d3ec80d42893ca1ad6324d49
szabomarcel <szabomarcell34@gmail.com>
2024-03-04 11:41:17 +0100

    orai

---
+++ book/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Book;
+
+namespace book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            books();
+            Console.WriteLine("Vége");
+            Console.ReadLine();
+        }
+        private static async Task books()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://openlibrary.org/api/books?bibkeys=ISBN:0201558025,LCCN:93005405&format=json");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
