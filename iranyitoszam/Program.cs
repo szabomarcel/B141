b394d696b418dd74ac3770e369ca2769db68fcff
szabomarcel <szabomarcell34@gmail.com>
2024-03-04 11:05:46 +0100

    restApi

---
+++ iranyitoszam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iranyitoszam
+{
+    internal class Program
+    {
+        static PostaCode postCode = null;
+        static void Main(string[] args)
+        {
+            string keresettIranyitoszam = "4200";
+            irszam(keresettIranyitoszam);
+            Console.WriteLine($"{postCode.Places[0].PlaceName.ToString()} település irányítószáma: {keresettIranyitoszam}");
+            Console.WriteLine("Vége");
+            Console.ReadLine();
+        }
+        private static async void irszam(string irszam)
+        {
+            string url = $"http://api.zippopotam.us/us/98121";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            string jsonString = await response.Content.ReadAsStringAsync();
+            postCode = PostaCode.FromJson(jsonString);
+        }
+    }
+}
