ab8d8040906d939b1ac16095a9b31b8c41df9281
szabomarcel <szabomarcell34@gmail.com>
2024-03-05 09:58:53 +0100

    orai

---
+++ School/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Univer;
+
+namespace School
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            List<University> schools = new List<University>();
+            schools = await universuty();
+            foreach (University school in schools)
+            {
+                Console.WriteLine($"{school.Name} - {school.Country}");
+            }
+            Console.WriteLine("Vége");
+            Console.ReadLine();
+        }           
+
+        private static async Task<List<University>> universuty()
+        {
+            List<University> school = new List<University>();
+            //HttpClient client = new HttpClient();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://universities.hipolabs.com/search?country=hungary");
+            var response = await client.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                string jsonSring = await response.Content.ReadAsStringAsync();
+                //var school = University.FromJson(jsonSring);
+            }
+            return school;
+        }
+    }
+}
--- iranyitoszam/Program.cs
+++ iranyitoszam/Program.cs
@@ -14,14 +14,16 @@ namespace iranyitoszam
         static void Main(string[] args)
         {
             string keresettIranyitoszam = "4200";
-            irszam(keresettIranyitoszam);
+            await irszam(keresettIranyitoszam);
             Console.WriteLine($"{postCode.Places[0].PlaceName.ToString()} település irányítószáma: {keresettIranyitoszam}");
+            Console.WriteLine($"{postCode.Places[0].State} megye");
+            Console.WriteLine($" Google Maps link: https://www.google.com/maps/place/{postCode.Places[0].Latitude},{postCode.Places[0].Longitude}");
             Console.WriteLine("Vége");
             Console.ReadLine();
         }
-        private static async void irszam(string irszam)
+        private static async Task irszam(string irszama)
         {
-            string url = $"http://api.zippopotam.us/us/98121";
+            string url = $"http://api.zippopotam.us/us/98121{irszama}";
             var client = new HttpClient();
             var request = new HttpRequestMessage(HttpMethod.Get, url);
             var response = await client.SendAsync(request);
